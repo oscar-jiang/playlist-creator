41c67edf51bff4de43b6b33f52f1a41d50d993e2
wilson <wilsonkmzhou@hotmail.com>
2026-01-05 01:16:34 -0800

    read dataset from URL and stored as a variable

--- backend/src/readDataset.py
+++ backend/src/readDataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 songDataset = pd.read_csv("https://raw.githubusercontent.com/oscar-jiang/playlist-creator/refs/heads/main/backend/dataset/tcc_ceds_music.csv")
+
 print(songDataset.head(3))
 
 
