54f325051878ee604a74580304c44349678408f6
wilson <wilsonkmzhou@hotmail.com>
2026-01-06 19:13:41 -0800

    read dataset from URL and stored as a variable

--- backend/src/readDataset.py
+++ backend/src/readDataset.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
-songDataset = pd.read_csv("https://raw.githubusercontent.com/oscar-jiang/playlist-creator/refs/heads/main/backend/dataset/tcc_ceds_music.csv")
+songDataset = pd.read_csv("https://raw.githubusercontent.com/oscar-jiang/playlist-creator/refs/heads/main/backend/dataset/tcc_ceds_music.csv") #reads data from url
 
-print(songDataset.head(3))
+pd.set_option("display.max_colwidth", 15) #configures the output dataframe in terminal
+
+
+songDataset.dropna(inplace = True) #drops NA values
+songDataset.drop_duplicates(inplace = True) #drops duplicate observations
+songDataset.drop(songDataset.columns[0], axis = 1, inplace = True) #drops the first column
+
+print(songDataset.tail(3)) #prints the last 3 observations of the dataframe
 
 
 
